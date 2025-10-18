4729181b278705b24e72b7e49c43f2670924e41b
Swarnava Bhattacharjee <92724359+Iam-007Swarna@users.noreply.github.com>
2025-10-19 02:31:51 +0530

    lint fix

--- src/components/marketplace/SearchBar.tsx
+++ src/components/marketplace/SearchBar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRouter, useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { Search, X, Command } from "lucide-react";
 import { useEffect, useRef, useState } from "react";
 import Link from "next/link";
